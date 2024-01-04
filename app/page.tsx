78ace061f8b4e0678d7e9280967aad835d10090d
gootibi <gootibi@gmail.com>
2024-01-04 15:18:12 +0100

    Changes to be committed: 	modified:   app/globals.css 	modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,29 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className=' h-screen Parnet p-4'>
-      <div className=' inline-block p-4 text-black bg-white rounded-lg border shadow-md shadow-cyan-500'>
-        <h1 className=' text-2xl'>Hello</h1>
-        <p className=' mb-4'>This is my div.</p>
-        <button className=' px-3 py-2 rounded-lg cursor-pointer bg-cyan-500 shadow-md shadow-cyan-500/50 hover:bg-red-500 hover:shadow-red-800 hover:text-white'>
-          Say Hello
-        </button>
+    <div>
+      <h1>This is a Title.</h1>
+      <h2>This is a Subtitle.</h2>
+      <p>This is a paragraph.</p>
+      <a href='https://freecodecamp.org'>This is a anchor.</a>
+      <div className='my-4'>
+        <button className='btn'>This is a Button</button>
       </div>
+      <div>
+        <input type="text" />
+      </div>
+      <div>
+        <input type="checkbox" id='checkbox' />
+        <label form="checkbox">CheckBox</label>
+      </div>
+      <div>
+        <select>
+          <option>Option 1</option>
+          <option>Option 2</option>
+        </select>
+      </div>
+      <h1 className='mt-2'>This is another Title.</h1>
+      <h2 className='mt-1'>This is another Subtitle.</h2>
     </div>
   )
 }
